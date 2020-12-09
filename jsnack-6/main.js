7be2a07c8a368949e9fa2a6b420f0f50cee5d839
Antonio <antonio.lovotrico@icloud.com>
2020-12-09 13:09:51 +0100

    added new constant Let & Const and arrow function to jsnacks 9 and 6

--- jsnack-6/main.js
+++ jsnack-6/main.js
@@ -1,15 +1,37 @@
 // jsnack 6 oggetti :  Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 
-var daGirare = "argomento";
+// var daGirare = "argomento";
 
-function rigira(stringa) {
-    var splitta = stringa.split("");
-    console.log(splitta.length); 
-    var reverse = "";
-    for (var i = splitta.length - 1; i >= 0; i--) {
-        reverse += splitta[i];    
+// function rigira(stringa) {
+//     var splitta = stringa.split("");
+//     console.log(splitta.length); 
+//     var reverse = "";
+//     for (var i = splitta.length - 1; i >= 0; i--) {
+//         reverse += splitta[i];    
+//     }
+//     console.log(reverse);
+// }
+
+// rigira(daGirare);
+
+
+
+// stesso codice ma con arrow function
+
+
+
+
+const daGirare = "argomento";
+
+const reverse = (stringa) => {
+    const splitta = stringa.split("");
+    console.log(splitta.length);
+    let reverse ="";
+    for (let i = splitta.length - 1; i >= 0; i--) {
+        reverse += splitta[i];
     }
-    console.log(reverse);
+    return reverse
 }
 
-rigira(daGirare);
+const result = reverse(daGirare);
+console.log(result);
--- jsnack-9/main.js
+++ jsnack-9/main.js
@@ -1,26 +1,62 @@
 //// JSnack 9 //// Scrivi una funzione che accetti tre argomenti:un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
 
+/*
+//creazione di un un array iniziale vuoto. Creato For loop per riempirlo di "i" elementi casuali.
 var arrIniziale = [];
 for (var i = 0; i < 10; i++) {
     var arrEle = Math.floor(Math.random() * (20 - 1)) + 1;
     arrIniziale.push(arrEle);    
 } 
+
+// creazione di due variabili con le caratteristiche assegnate
 var b = Math.floor(Math.random() * (arrIniziale.length - 1)) + 1; 
 var a = Math.floor(Math.random() * (b - 2)) + 1;
-
 
 console.log(arrIniziale);
 console.log(a);
 console.log(b);
+
+// dichiarazione dell'array finale da riempire tramite la funzione creata
 var arrFinale = [];
 
 function arrCreation(arrDue,num1,num2){
     for (var j = num1; j < num2; j++){
-        var arrFinal = Math.floor(Math.random() * (20 - 1)) + 1;
-        arrDue.push(arrFinal);
+        arrDue.push(arrIniziale[j]);
     }
     return arrDue
 }
 
 var result = arrCreation(arrFinale,a,b);
+console.log(result);
+*/
+
+
+// Stesso esercizio ma sviluppato con Const Let ed Arrow function
+
+//creazione di un un array iniziale vuoto. Creato For loop per riempirlo di "i" elementi casuali.
+const arrIniziale = [];
+for (let i = 0; i < 10; i++) {
+    const arrEle = Math.floor(Math.random() * (20 - 1)) + 1;
+    arrIniziale.push(arrEle);    
+} 
+
+// creazione di due variabili con le caratteristiche assegnate
+const b = Math.floor(Math.random() * (arrIniziale.length - 1)) + 1; 
+const a = Math.floor(Math.random() * (b - 2)) + 1;
+
+console.log(arrIniziale);
+console.log(a);
+console.log(b);
+
+// dichiarazione dell'array finale da riempire tramite la funzione creata
+const arrFinale = [];
+
+const arrCreation = (arrDue,num1,num2) => {
+    for (let j = num1; j < num2; j++){
+        arrDue.push(arrIniziale[j]);
+    }
+    return arrDue
+}
+
+const result = arrCreation(arrFinale,a,b);
 console.log(result);
